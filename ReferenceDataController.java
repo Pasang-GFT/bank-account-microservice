6a8133ada2d16f59a671ed4034368ba3cb1f4d65
Pasanghh <86737740+Pasanghh@users.noreply.github.com>
2024-03-25 13:12:55 +0000

    controller

---
+++ ReferenceDataController.java
@@ -0,0 +1,40 @@
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/referenceData")
+public class ReferenceDataController {
+
+    private final ReferenceDataService referenceDataService;
+
+    public ReferenceDataController(ReferenceDataService referenceDataService) {
+        this.referenceDataService = referenceDataService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ReferenceData>> getAllReferenceData() {
+        List<ReferenceData> referenceData = referenceDataService.getAllReferenceData();
+        return new ResponseEntity<>(referenceData, HttpStatus.OK);
+    }
+
+    @PutMapping
+    public ResponseEntity<ReferenceData> updateReferenceData(@RequestBody ReferenceData referenceData) {
+        ReferenceData updatedReferenceData = referenceDataService.updateReferenceData(referenceData);
+        return new ResponseEntity<>(updatedReferenceData, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<ReferenceData> createReferenceData(@RequestBody ReferenceData referenceData) {
+        ReferenceData createdReferenceData = referenceDataService.createReferenceData(referenceData);
+        return new ResponseEntity<>(createdReferenceData, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteReferenceData(@PathVariable String id) {
+        referenceDataService.deleteReferenceData(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
